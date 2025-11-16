bfd40f73d909be811fbc96924b2f7be4dcdc82b0
anshikabansal04 <anshikab0013@gmail.com>
2025-11-16 16:14:59 +0530

    Add heap class with insert and heapsort methods
    Implement heap data structure and heapsort algorithm.

---
+++ labassignment8dsa/que5.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+class heap{
+    public:
+    int arr[100];
+    int size;
+    public:
+    heap(){
+        arr[0]=-1;
+        size=0;
+    }
+    void insert(int val){
+        size++;
+        int index=size;
+        arr[index]=val;
+        int parentindex=index/2;
+        while(index>0){
+            if(arr[parentindex]<arr[index]){
+                swap(arr[parentindex],arr[index]);
+            }
+            else{
+                return ;
+            }  
+        }
+    }
+    void heapify(int arr[],int n,int i){
+int largest=i;
+int left=2*i;
+int right=2*i+1;
+if(left<=n && arr[largest]<arr[left]){
+    largest=left;
+}
+if(right<=n && arr[largest]<arr[right]){
+    largest=right;
+}
+if(largest!=i){
+    swap(arr[largest],arr[i]);
+    heapify(arr,n,largest);
+}
+        } 
+void heapsort(int arr[],int n){
+    int size=n;
+    while(size>1){
+        swap(arr[size],arr[1]);
+        size--;
+        heapify(arr,size,1);
+    }
+}
+};
+int main(){
+    heap h;
+    int arr[6]={-1,54,53,55,52,50};
+    int n=5;
+for(int i=n/2;i>0;i--){//o(nlog(n))
+        h.heapify(arr,n,i);
+    }
+for(int i=1;i<=n;i++){
+    cout<<arr[i]<<endl;
+}
+h.heapsort(arr,n);
+cout<<"pritng"<<endl;
+for(int i=1;i<=n;i++){
+    cout<<arr[i]<<endl;
+}
+return 0;
+}
